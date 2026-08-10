7456f43730a4212e87466ed61e875484a62d5792
KeyForge-Installer <securityinternetionas@gmail.com>
2026-08-10 21:34:45 +0600

    Add files via upload

---
+++ js/route-manifest-download-guidance.2ab55a8a.js
@@ -0,0 +1,5 @@
+
+            ;(function(){
+              window._MODERNJS_ROUTE_MANIFEST = {"routeAssets":{"desktop-video-editor":{"chunkIds":["408","783","493","535","216","59","140","309","84"],"assets":["static/js/builder-runtime.86cf622b.js","static/js/lib-react.13e55cd9.js","static/js/lib-polyfill.9eb748d8.js","static/js/lib-router.6ce1f445.js","static/js/argus-builder-strategy.3530d6f1.js","static/js/59.e4e6f725.js","static/js/140.711aa151.js","static/js/309.7425fa98.js","static/js/desktop-video-editor.03f9d62e.js"],"referenceCssAssets":[]}}};
+            })();
+          
